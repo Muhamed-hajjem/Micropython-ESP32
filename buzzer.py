568ada6ac5ddb94560cbd39c51e1cd1d089e180e
Muhamed-hajjem <127667459+Muhamed-hajjem@users.noreply.github.com>
2025-02-26 00:56:20 +0100

    buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -2,8 +2,10 @@ from machine import Pin as pin ,PWM
 import time , utime 
 bips = PWM(pin(15))
 bips.freq(6000)
-def bip (x):
+def bip ():
     bips.duty_u16(65533)
-    time.sleep(x)
+    time.sleep(0.5)
     bips.duty_u16(0)
 
+while True :
+    bip()
