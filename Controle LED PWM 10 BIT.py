e062c4e273070857ada889b29011f7815b39d1f8
Muhamed-hajjem <127667459+Muhamed-hajjem@users.noreply.github.com>
2025-02-26 00:47:51 +0100

    Update Controle LED PWM 10 BIT.py

--- Controle LED PWM 10 BIT.py
+++ Controle LED PWM 10 BIT.py
@@ -5,9 +5,8 @@ from time import sleep
 led = PWM(Pin(33))
 led.freq(500)  
 
-# Faire varier la luminosité
 while True:
-    for duty in range(0, 1024):  # Duty cycle de 0 à 1023
+    for duty in range(0, 1024):  
         led.duty(duty)
         sleep(0.01)
     for duty in range(1023, -1, -1):
